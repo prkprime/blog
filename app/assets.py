5ef9f39e89a39647c14034132e874e6811722b04
Pratik Gorade <pratikgorade0@gmail.com>
2020-08-11 16:55:55 +0530

    added some more type hinting
    Signed-off-by: Pratik Gorade <pratikgorade0@gmail.com>

--- app/assets.py
+++ app/assets.py
@@ -1,24 +1,25 @@
 import mistune
 import re
 from app.highlightrenderer import HighlightRenderer
+from typing import Union
 
 class Post:
     '''
     this class stores the bellow mentioned metadata of blogpost,
     markdown and HTML converted content of blog
     '''
-    def __init__(self, title, date, summary, href, md_content=None):
-        self.title = title
-        self.date = date
-        self.summary = summary
-        self.href = href
-        self.md_content = md_content
+    def __init__(self, title: str, date: str, summary: str, href: str, md_content: Union[str, None] = None) -> None:
+        self.title: str = title
+        self.date: str = date
+        self.summary: str = summary
+        self.href: str = href
+        self.md_content: Union[str, None] = md_content
         if md_content == None:
-            self.html_content = None
+            self.html_content: Union[mistune.Markdown, None] = None
         else:
-            self.html_content = md_to_html(md_content)
+            self.html_content:  Union[mistune.Markdown, None] = md_to_html(md_content)
 
-def parse_markdown_post(md_path: str, meta=False) -> Post:
+def parse_markdown_post(md_path: str, meta=False) -> Union[Post, None]:
     '''
     :param md_path -> path to markdown file
     :param meta -> set True if you want to retrive only meta data for index page
